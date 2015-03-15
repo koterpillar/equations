7b93dbb28c0c1ea1b101d725839c54bfd4469c2e
Alexey Kotlyarov <a@koterpillar.com>
2015-03-15 15:46:25 +1100

    Get rid of operand multipliers

--- src/Control/Equation.hs
+++ src/Control/Equation.hs
@@ -1,20 +1,11 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Equation (
-    Expression (..),
-    Equation (..),
+    Expression,
+    Equation,
     (=:=),
     evar,
-    econst,
     solveLinear,
 ) where
 
-import Control.Lens (Lens')
-
 import Control.Equation.Solve (solveLinear)
 import Control.Equation.Types
-
-evar :: Num value => Lens' world value -> Expression world value
-evar var = Expression [(fromInteger 1, Variable var)]
-
-econst :: Num value => value -> Expression world value
-econst c = Expression [(fromInteger 1, Constant c)]
--- src/Control/Equation/Types.hs
+++ src/Control/Equation/Types.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Equation.Types (
-    Operand (..),
     Expression (..),
     Equation (..),
+    evar,
     (=:=),
 ) where
 
@@ -10,11 +10,15 @@ import Control.Lens (Lens')
 
 import Data.Maybe
 
-data Operand world value = Constant value | Variable (Lens' world value)
-
 -- TODO: Non-linear expressions
 
-data Expression world value = Expression { unExpression :: [(value, Operand world value)] }
+-- Operands are either constants, or variables multiplied by constants
+data Term world value = Constant value | Variable value (Lens' world value)
+
+data Expression world value = Expression { unExpression :: [Term world value] }
+
+evar :: Num value => Lens' world value -> Expression world value
+evar var = Expression [Variable 1 var]
 
 -- TODO: Is there a LinearSpace typeclass?
 instance Num value => Num (Expression world value) where
@@ -23,7 +27,7 @@ instance Num value => Num (Expression world value) where
             else if isConstant a then b * a
             else error "Cannot multiple two non-constant expressions"
 
-    fromInteger i = Expression [(fromInteger 1, Constant (fromInteger i))]
+    fromInteger i = Expression [Constant $ fromInteger i]
 
     negate = constMul (-1)
 
@@ -31,19 +35,19 @@ instance Num value => Num (Expression world value) where
     abs = id
     signum = const 1
 
-cvalue :: Num value => (value, Operand world value) -> Maybe value
-cvalue (i, Constant j) = Just $ i * j
+cvalue :: Num value => Term world value -> Maybe value
+cvalue (Constant j) = Just j
 cvalue _ = Nothing
 
 -- Simplify the expression by cancelling out the constants.
 -- The variables cannot be cancelled out because they cannot be compared to
 -- each other.
 simplify :: Num value => Expression world value -> Expression world value
-simplify (Expression exp) = Expression $ [(fromInteger 1, Constant csum)] ++ vars
+simplify (Expression exp) = Expression $ Constant csum:vars
     where vars = filter isVarExp exp
           csum = sum $ catMaybes $ map cvalue $ filter (not . isVarExp) exp
-          isVarExp (_, Constant _) = True
-          isVarExp (_, Variable _) = False
+          isVarExp (Constant _) = True
+          isVarExp _ = False
 
 constantValue :: Num value => Expression world value -> Maybe value
 constantValue = fmap sum . sequence . map cvalue . unExpression
@@ -53,7 +57,8 @@ isConstant = isJust . constantValue
 
 constMul :: Num value => value -> Expression world value -> Expression world value
 constMul v = Expression . map (opMul v) . unExpression
-    where opMul i (j, k) = (i * j, k)
+    where opMul i (Constant j) = Constant $ i * j
+          opMul i (Variable j k) = Variable (i * j) k
 
 data Equation world value = Equation (Expression world value)
 
