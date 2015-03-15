bad31a9992875f3fd33e9f0e3a9ee573a7f65c95
Alexey Kotlyarov <a@koterpillar.com>
2015-03-15 15:39:30 +1100

    Flesh out Expression operations

--- src/Control/Equation/Types.hs
+++ src/Control/Equation/Types.hs
@@ -8,27 +8,32 @@ module Control.Equation.Types (
 
 import Control.Lens (Lens')
 
+import Data.Maybe
+
 data Operand world value = Constant value | Variable (Lens' world value)
 
 -- TODO: Non-linear expressions
 
-data Expression world value = Expression [(value, Operand world value)]
+data Expression world value = Expression { unExpression :: [(value, Operand world value)] }
 
 -- TODO: Is there a LinearSpace typeclass?
 instance Num value => Num (Expression world value) where
     Expression a + Expression b = simplify $ Expression (a ++ b)
-    Expression a * b = case simplify b of
-                           -- TODO: either a or b can be constant
-                           Expression [(c, Constant v)] -> Expression $ map (opMul (c * v)) a
-                           _ -> error "Cannot multiple two non-constant expressions"
+    a * b = if isConstant b then constMul (fromJust $ constantValue b) a
+            else if isConstant a then b * a
+            else error "Cannot multiple two non-constant expressions"
 
     fromInteger i = Expression [(fromInteger 1, Constant (fromInteger i))]
 
-    negate (Expression a) = Expression $ map (opMul (fromInteger (-1))) a
+    negate = constMul (-1)
 
     -- These have to correspond to each other
     abs = id
     signum = const 1
+
+cvalue :: Num value => (value, Operand world value) -> Maybe value
+cvalue (i, Constant j) = Just $ i * j
+cvalue _ = Nothing
 
 -- Simplify the expression by cancelling out the constants.
 -- The variables cannot be cancelled out because they cannot be compared to
@@ -36,13 +41,19 @@ instance Num value => Num (Expression world value) where
 simplify :: Num value => Expression world value -> Expression world value
 simplify (Expression exp) = Expression $ [(fromInteger 1, Constant csum)] ++ vars
     where vars = filter isVarExp exp
-          csum = sum $ map cvalue $ filter (not . isVarExp) exp
+          csum = sum $ catMaybes $ map cvalue $ filter (not . isVarExp) exp
           isVarExp (_, Constant _) = True
           isVarExp (_, Variable _) = False
-          cvalue (i, Constant j) = i * j
 
-opMul :: Num value => value -> (value, Operand world value) -> (value, Operand world value)
-opMul i (j, k) = (i * j, k)
+constantValue :: Num value => Expression world value -> Maybe value
+constantValue = fmap sum . sequence . map cvalue . unExpression
+
+isConstant :: Num value => Expression world value -> Bool
+isConstant = isJust . constantValue
+
+constMul :: Num value => value -> Expression world value -> Expression world value
+constMul v = Expression . map (opMul v) . unExpression
+    where opMul i (j, k) = (i * j, k)
 
 data Equation world value = Equation (Expression world value)
 
