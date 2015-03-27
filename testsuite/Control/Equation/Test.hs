0cb257d43b498259287ac8ba342d5ab5c9e05961
Alexey Kotlyarov <a@koterpillar.com>
2015-03-27 20:58:38 +1100

    Test for solution status

--- testsuite/Control/Equation/Test.hs
+++ testsuite/Control/Equation/Test.hs
@@ -27,7 +27,9 @@ test_solveLinear = do
 
     -- TODO: the initial state is meaningless
     let playground = Playground 0 0 0
-    let solution = execState (solveLinear equations) playground
+    let (result, solution) = runState (solveLinear equations) playground
+
+    result @?= SingleSolution
 
     solution ^. sheep + solution ^. cows @?= 20
     solution ^. cows + solution ^. chicken @?= 30
@@ -37,3 +39,21 @@ test_solveLinear = do
     solution ^. chicken @?= 25
     solution ^. sheep @?= 15
     solution ^. cows @?= 5
+
+test_no_solutions = do
+    let equations = [ evar _1 =:= 1
+                    , evar _1 =:= 2
+                    ]
+
+    let result = evalState (solveLinear equations) (0, 0)
+
+    result @?= NoSolutions
+
+test_multiple_solutions = do
+    let equations = [ evar _1 =:= evar _2
+                    , evar _2 =:= evar _1
+                    ]
+
+    let result = evalState (solveLinear equations) (0, 0)
+
+    result @?= MultipleSolutions
